f725923b655f33d24ccac060fcc87f8e05255397
Bhavani Prakash <bhavaniprakash960@gmail.com>
2026-02-11 21:12:01 +0530

    back to the nodemailer

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,38 +1,41 @@
-import { Resend } from "resend";
-import dotenv from "dotenv";
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+
 
 dotenv.config();
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+ 
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
 
 export const sendConfirmationEmail = async (to, name, eventName) => {
-  try {
-    await resend.emails.send({
-      from: "ECLECTICA 2K26 Events <onboarding@resend.dev>",
-      to: to,
-      subject: `Registration Confirmed – ${eventName}`,
-      html: `
-        <h2>Hello ${name} 👋</h2>
-        <p>We’re excited to tell you that your registration for <b>${eventName}</b> at <b>ECLECTICA 2k26</b> has been successfully confirmed! ✨</p>
-        <p>You’re officially part of something unforgettable.</p>
-        <br/>
+    const mailOptions = {
+        from: `"ECLECTICA 2K26- Events" <${process.env.EMAIL_USER}>`,
+        to,
+        subject: `Registration Confirmed – ${eventName}`,
+        html: `
+      <h2>Hello ${name} 👋</h2>
+      <p>We’re excited to tell you that your registration for <b>${eventName}</b>  at ECLECTICA 2k26 has been successfully confirmed! ✨</p>
+      <p> You’re officially part of something unforgettable.</p>
+      <br/>
+          <p>  📍 Event: <b>ECLECTICA 2k26</b> – Department Fest <p>
+          <p>  📅 Date: March 13, 2026</>
+          <p>  📌 Venue: College Campus MITS -Lakshmi Block</p>
+          <p>  🎯 Registered Event: <b>${eventName}</b></p>
+      </pre>
+      <p> If you have any questions or need help, feel free to reach out to us anytime. We’re always happy to help 😊 </p>
+      <p> <b>Phone :</b> +91 86884 97800 </p><br />
 
-        <p>📍 <b>Event:</b> ECLECTICA 2k26 – Department Fest</p>
-        <p>📅 <b>Date:</b> March 13, 2026</p>
-        <p>📌 <b>Venue:</b> College Campus MITS – Lakshmi Block</p>
-        <p>🎯 <b>Registered Event:</b> ${eventName}</p>
+      <p> FROM —  Team <b>ECLECTICA 2K26 </b></p>
+    `
+    };
 
-        <br/>
-        <p>If you have any questions or need help, feel free to reach out to us anytime. We’re always happy to help 😊</p>
-        <p><b>Phone:</b> +91 86884 97800</p>
-
-        <br/>
-        <p>FROM — Team <b>ECLECTICA 2K26</b></p>
-      `,
-    });
-
-    console.log("Confirmation email sent to:", to);
-  } catch (error) {
-    console.error("Resend email error:", error);
-  }
+    await transporter.sendMail(mailOptions);
 };
