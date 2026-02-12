5463037e670312af016f1580baad5e9b456e73ab
Bhavani Prakash <bhavaniprakash960@gmail.com>
2026-02-12 18:01:36 +0530

    ETIMEDOUT fixed in sendmail.js file

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -7,8 +7,12 @@ dns.setDefaultResultOrder("ipv4first");
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
- 
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    logger: true,
+    debug:true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
